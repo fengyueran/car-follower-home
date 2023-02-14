949508f4d12e5c1933fbff5b8331768a0d4fc9dc
xinghunm <xinghunm@keyayun.com>
2023-02-14 18:05:10 +0800

    change file structure

--- client/src/features/nav-bar/nav-bar-container.tsx
+++ client/src/features/nav-bar/nav-bar-container.tsx
@@ -1,7 +1,7 @@
 import { View } from "@tarojs/components";
 import { styled } from "linaria/react";
 
-import NavBar from "../nav-bar";
+import NavBar from "./nav-bar";
 
 const Container = styled(View)`
   position: fixed;
